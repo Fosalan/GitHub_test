56fa5586bfde69ef82d8995882a29ff39d0a6ea7
Niels Corbeel <niels.corbeel@student.hu.nl>
2018-09-18 14:05:32 +0200

    iets met leeftijden

---
+++ Les4/oefening leeftijd.py
@@ -0,0 +1,8 @@
+name = (input('Enter your name:'))
+age = int(input('Enter your age:'))
+
+if age >= 18:
+    print(name + ' you are allowed to vote')
+else:
+    print(name + ' you are not allowed to vote')
+
