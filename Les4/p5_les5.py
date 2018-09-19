074bb4d2b153f266ede47acac7403022e80ed3b0
Niels Corbeel <niels.corbeel@student.hu.nl>
2018-09-19 16:30:39 +0200

    opdracht 5 in les 5

---
+++ Les4/p5_les5.py
@@ -0,0 +1,15 @@
+for index in range(11):
+    print(index)
+
+for index2 in range(1,10):
+    print(index2)
+
+for index3 in range(0,9,2):
+    print(index3)
+
+for index4 in range(1,10,2):
+    print(index4)
+
+for index5 in range(20,61,10):
+    print(index5)
+
